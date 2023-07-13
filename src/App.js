8a03cd3f2b417a3195d3d45d136ac0f0a638892b
sahlminkok <sahlminkok7@gmail.com>
2023-07-13 10:53:38 +0100

    Set up the router and links

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import CryptosList from './components/CryptosList';
+import Navbar from './components/Navbar';
+import Details from './components/Details';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<CryptosList />} />
+        <Route path="/:id" element={<Details />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
